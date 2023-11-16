eea2761b8d1872c98b24b63d48ed9852c0f651a1
MUMER123 <uumer2428@gmail.com>
2023-11-16 16:00:30 +0500

    Print odd numbers till entered number

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        MyThread t = new MyThread("odd_till");
+        System.out.println("Enter a positive integer: ");
+        int num = input();
+        MyThread t = new MyThread("odd_till",num);
         t.start();
+    }
+    public static int input(){
+        Scanner s = new Scanner(System.in);
+        int number = s.nextInt();
+        if(number<0){
+            System.out.println("Only Positive Integers Allowed...");
+            return  input();
+        }
+        else{
+            return number;
+        }
     }
 }
--- src/MyThread.java
+++ src/MyThread.java
@@ -3,28 +3,17 @@ import java.util.Scanner;
 
 public class MyThread extends Thread{
     private int num;
-    MyThread(String name){
+    MyThread(String name,int num){
         super(name);
+        this.num =  num;
     }
     @Override
     public void run(){
-        System.out.println("Enter a positive Integer: ");
-        this.num = input();
         for(int i=0;i<=num;i++){
             if(!(i%2==0)){
                 System.out.println(i);
             }
         }
     }
-    public int input(){
-        Scanner s = new Scanner(System.in);
-        int number = s.nextInt();
-        if(num<0){
-            System.out.println("Only Positive Integers Allowed...");
-            return  input();
-        }
-        else{
-            return number;
-        }
-    }
+
 }
